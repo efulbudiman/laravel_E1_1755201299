52fb381438b3c674d4506e0dfa0e548dd9afe384
efulbudiman <efulbudiman17@gmail.com>
2020-08-11 10:41:17 +0700

    UAS Fitur Mata Kuliah

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'MahasiswaController@index');
-//Mahasiswa (Route dengan detail satu persatu)
 Route::get('/mhs', 'MahasiswaController@index')->name('mhs.index');
 Route::get('/mhs/list', 'MahasiswaController@mhs_list')->name('mhs.list');
 Route::get('/mhs/create', 'MahasiswaController@create');
@@ -22,5 +21,6 @@ Route::post('/mhs/store', 'MahasiswaController@store');
 Route::get('/mhs/edit/{nim}', 'MahasiswaController@edit');
 Route::put('/mhs/update/{mahasiswa:nim}', 'MahasiswaController@update')->name('mhs.update');
 Route::get('/mhs/delete/{mahasiswa:nim}', 'MahasiswaController@destroy')->name('mhs.delete');
-//Prodi (Route Framework)
+
 Route::resource('/prodi', 'ProdiController'); 
+Route::resource('/matkul', 'MatkulController'); 
